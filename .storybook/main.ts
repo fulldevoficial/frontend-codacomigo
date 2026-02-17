ec646e537dd6b2f44d96d21db5439ab7e0fc89aa
Fernando Valença <fernandovalenca2013@gmail.com>
2026-02-16 21:54:19 -0300

    fix(ci): corrigindo problemas do ci

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,6 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-a11y', '@storybook/addon-docs', '@storybook/addon-vitest'],
   framework: '@storybook/nextjs-vite',
-  staticDirs: ['..\\public'],
+  staticDirs: ['../public'],
 };
 export default config;
