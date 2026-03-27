f0d1ab2e8c8c6b5456aebcd901ae590571dddcb9
Mauricio Antero <mjunior447@gmail.com>
2026-03-26 21:15:20 -0300

    fix: corrige erro de lint na pipeline

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
-import type { Viewport, Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "Frontend Coda Comigo",
