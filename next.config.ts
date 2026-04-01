a34d846727e60eed31441203719824f921f260cc
Maria Clara Silva <maclara2018@gmail.com>
2026-04-01 20:26:09 -0300

    correção pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
+import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest.json$/],
+});
 
-export default nextConfig;
+const nextConfig: NextConfig = {};
+
+export default pwaConfig(nextConfig);
