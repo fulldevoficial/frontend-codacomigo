c994ecbfe844160207bc3b7e37e64019c8ee99ae
Lohluizesd <lohluizesd@gmail.com>
2026-04-14 22:32:18 -0300

    feat(button): adiciona variante social para os botões de login

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,11 +13,14 @@ const buttonVariants = cva(
         destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+        social:
+          'bg-zinc-100 border border-zinc-300 text-zinc-500 shadow-sm hover:bg-zinc-200 cursor-pointer rounded-x1',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-9 px-4 py-2',
+        social: 'h-14 px-8',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9',
@@ -33,12 +36,20 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  icon?: React.ReactNode;
+  label?: string;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, icon, label, children, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+
+    return (
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+        {icon}
+        {children ?? label}
+      </Comp>
+    );
   },
 );
 Button.displayName = 'Button';
